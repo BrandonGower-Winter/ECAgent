6356e6317e0fe42cb1ae9c731e666cda843f5e3b
Brandon Gower-Winter <brandongowerwinter@gmail.com>
2020-05-27 17:25:46 +0200

    Added CubeWorld to the environments sub-package. CubeWorld is a 3D discrete environment

--- ECAgent/Environments.py
+++ ECAgent/Environments.py
@@ -120,3 +120,61 @@ class GridWorld(Environment):
 
     def getDimensions(self):
         return self.width, self.height
+
+
+class CubeWorld(Environment):
+    """ CubeWorld is a discrete environment with 3 axes (x,y,z-axes). It can be used in place of the base Environment
+    class. All agents added to a GridWorld class are given a PositionComponent to denote their place in the world.
+
+    A CubeWorld's dimensions are defined by a width, height and depth properties.
+
+    CubeWorld.addComponent(comp) adds component comp to each of the cells in the environment."""
+
+    def __init__(self, width, height, depth, id: str = 'ENVIRONMENT'):
+
+        if width < 1 or height < 1 or depth < 1:
+            raise Exception("Cannot create a CubeWorld with a negative width or height.")
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.depth = depth
+        self.cells = []
+
+        # Create cells
+        for z in range(depth):
+            for y in range(height):
+                for x in range(width):
+                    self.cells.append(Agent(str(x + self.width * (y + self.depth * z)), None))
+
+    def addAgent(self, agent: Agent, xPos: int = 0, yPos: int = 0, zPos: int = 0.0):
+        """Adds an agent to the environment. Overrides the base class function.
+        This function will also add a PositionComponent to the agent object.
+        If the xPos, yPos or zPos is greater than the width of the world, an error will be thrown."""
+
+        if xPos >= self.width or xPos < 0 or yPos >= self.height or yPos < 0 or zPos >= self.depth or zPos < 0:
+            raise Exception("Cannot add the Agent to position not on the map.")
+
+        agent.addComponent(PositionComponent(agent, agent.model, x=xPos, y=yPos, z=zPos))
+        super().addAgent(agent)
+
+    def removeAgent(self, agentID: str):
+        """ Removes the agent from the environment. Will also remove the PositionComponent from the agent"""
+        if agentID in self.agents:
+            self.agents[agentID].removeComponent(PositionComponent)
+
+        super().removeAgent(agentID)
+
+    def setModel(self, model: Model):
+        super().setModel(model)
+
+        for cell in self.cells:
+            cell.model = model
+
+    def getAgentsAt(self, xPos: int, yPos: int, zPos: int):
+        """Returns a list of agents at position xPos. Will return [] empty if no agents are in that cell"""
+        return [self.agents[agentKey] for agentKey in self.agents
+                if self.agents[agentKey][PositionComponent].x == xPos and self.agents[agentKey][PositionComponent].y == yPos and self.agents[agentKey][PositionComponent].z == zPos]
+
+    def getDimensions(self):
+        return self.width, self.height, self.depth
--- tests/test_ECAgentEnvironments.py
+++ tests/test_ECAgentEnvironments.py
@@ -202,7 +202,139 @@ class TestGridWorld:
         # Test non empty case
         assert model.environment.getAgentsAt(0,0) == [agent]
 
+    def test_setModel(self):
+        model = Model()
+        env = GridWorld(5, 5)
+
+        assert env.model is not model
+        # Test for all cells:
+        for x in range(25):
+            assert env.cells[x].model is None
+
+        env.setModel(model)
+        assert env.model is model
+
+        # Test for all model for all cells
+        for x in range(25):
+            assert env.cells[x].model is model
+
     def test_getDimensions(self):
         env = GridWorld(3,5)
 
         assert env.getDimensions() == (3,5)
+
+
+class TestCubeWorld:
+
+    def test__init__(self):
+        # Test failed initialization
+        with pytest.raises(Exception):
+            CubeWorld(0, 5, 5)
+
+        with pytest.raises(Exception):
+            CubeWorld(5, 0, 5)
+
+        with pytest.raises(Exception):
+            CubeWorld(5, 5, 0)
+
+        # Test default init()
+
+        env = CubeWorld(5, 5, 5)
+        assert env.width == 5
+        assert env.height == 5
+        assert env.depth == 5
+        assert len(env.cells) == 125
+        assert env.id == 'ENVIRONMENT'
+        assert env.model is None
+        assert len(env.agents) == 0
+
+        for i in range(len(env.cells)):
+            assert env.cells[i].model is None
+            assert env.cells[i].id == str(i)
+
+    def test_addAgent(self):
+        model = Model(CubeWorld(5, 5, 5))
+        agent = Agent("a1", model)
+
+        # Test case when agent is added outside of the environment's dimensions [<0]
+        with pytest.raises(Exception):
+            model.environment.addAgent(agent, -1, 0, 0)
+
+        with pytest.raises(Exception):
+            model.environment.addAgent(agent, 0, -1, 0)
+
+        with pytest.raises(Exception):
+            model.environment.addAgent(agent, 0, 0, -1)
+
+        # Test case when agent is added outside of the environment's dimensions [>width]
+        with pytest.raises(Exception):
+            model.environment.addAgent(agent, 5, 0, 0)
+
+        with pytest.raises(Exception):
+            model.environment.addAgent(agent, 0, 5, 0)
+
+        with pytest.raises(Exception):
+            model.environment.addAgent(agent, 0, 0, 5)
+
+        # Test default case
+        model.environment.addAgent(agent)
+        assert len(model.environment.agents) == 1
+        assert model.environment.getAgent(agent.id) == agent
+        assert agent[PositionComponent].x == 0 and agent[PositionComponent].y == 0 and agent[PositionComponent].z == 0
+
+        # Test when agent is added twice
+        with pytest.raises(Exception):
+            model.environment.addAgent(agent)
+
+        # Test case when position is specified
+        model.environment.removeAgent(agent.id)
+
+        model.environment.addAgent(agent, 1, 2, 3)
+        assert len(model.environment.agents) == 1
+        assert model.environment.getAgent(agent.id) == agent
+        assert agent[PositionComponent].x == 1 and agent[PositionComponent].y == 2 and agent[PositionComponent].z == 3
+
+    def test_removeAgent(self):
+        model = Model(CubeWorld(5, 5, 5))
+        agent = Agent("a1", model)
+        model.environment.addAgent(agent)
+        model.environment.removeAgent(agent.id)
+        # Test removal
+        assert len(model.environment.agents) == 0
+        assert agent[PositionComponent] is None
+        # Test removal when agent doesn't exist in t
+        with pytest.raises(Exception):
+            model.environment.removeAgent(agent.id)
+
+    def test_setModel(self):
+        model = Model()
+        env = CubeWorld(5, 5, 5)
+
+        assert env.model is not model
+        # Test for all cells:
+        for x in range(125):
+            assert env.cells[x].model is None
+
+        env.setModel(model)
+        assert env.model is model
+
+        # Test for all model for all cells
+        for x in range(125):
+            assert env.cells[x].model is model
+
+    def test_getAgentsAt(self):
+        model = Model(CubeWorld(5, 5, 5))
+        agent = Agent("a1", model)
+
+        model.environment.addAgent(agent, 0, 0, 0)
+
+        # Test empty case
+        assert model.environment.getAgentsAt(5, 5, 5) == []
+
+        # Test non empty case
+        assert model.environment.getAgentsAt(0, 0, 0) == [agent]
+
+    def test_getDimensions(self):
+        env = CubeWorld(1, 2, 3)
+
+        assert env.getDimensions() == (1, 2, 3)

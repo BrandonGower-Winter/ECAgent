b254754d8a60e78eacc5b93092eee4006c138352
Brandon Gower-Winter <brandongowerwinter@gmail.com>
2021-01-21 18:23:53 +0200

    chore: Modified setup.py to read package details from package.json

--- setup.py
+++ setup.py
@@ -1,25 +1,17 @@
+import json
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("package.json", 'r') as fh:
-    version_number = fh.read()
+with open('package.json') as json_file:
+    data = json.load(json_file)
 
-setup(
-    name="ECAgent",
-    version='0.0.6',
-    author="Brandon Gower-Winter",
-    author_email="brandongowerwinter@gmail.com",
-    description="An Agent-based Modelling framework based on the Entity-Component-System architectural pattern.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/BrandonGower-Winter/ABMECS",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+data.update({
+    'long_description': long_description,
+    'long_description_content_type': 'text/markdown',
+    'packages': find_packages()
+})
+
+setup(**data)
